aad7fd929ca1524042cf11a090b1629ed27e3778
JadenJSJ <44781632+JadenJSJ@users.noreply.github.com>
2024-07-17 16:28:56 +0700

    Update Magisk Delta to R6687BB53-kitsune

--- usr/magisk/update_magisk.sh
+++ usr/magisk/update_magisk.sh
@@ -9,18 +9,18 @@ ver="$(cat "$DIR/magisk_version" 2>/dev/null || echo -n 'none')"
 if [ "x$1" = "xcanary" ]
 then
 	nver="canary"
-	magisk_link="https://github.com/HuskyDG/magisk-files/releases/download/1707294287/app-release.apk"
+	magisk_link="https://github.com/HuskyDG/magisk-files/releases/download/1720171347/app-release.apk"
 elif [ "x$1" = "xlocal" ]
 then
 	nver="local"
-	magisk_link="https://github.com/HuskyDG/magisk-files/releases/download/1707294287/app-release.apk"
+	magisk_link="https://github.com/HuskyDG/magisk-files/releases/download/1720171347/app-release.apk"
 else
 	if [ "x$1" = "x" ]; then
 		nver="$(curl -s https://raw.githubusercontent.com/topjohnwu/magisk-files/master/stable.json | jq '.magisk.version' | cut -d '"' -f 2)"
 	else
 		nver="$1"
 	fi
-	magisk_link="https://github.com/HuskyDG/magisk-files/releases/download/1707294287/app-release.apk"
+	magisk_link="https://github.com/HuskyDG/magisk-files/releases/download/1720171347/app-release.apk"
 fi
 
 if [ \( -n "$nver" \) -a \( "$nver" != "$ver" \) -o ! \( -f "$DIR/arm/magiskinit64" \) -o \( "$nver" = "canary" \) -o \( "$nver" = "local" \) ]
